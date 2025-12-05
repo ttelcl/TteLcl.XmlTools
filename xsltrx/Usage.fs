93c6d05b15264587846267d58833b2b29a5064ca
ttelcl <9429819+ttelcl@users.noreply.github.com>
2025-12-05 23:26:33 +0200

    Small Usage update

--- xsltrx/Usage.fs
+++ xsltrx/Usage.fs
@@ -5,6 +5,7 @@ open CommonTools
 open ColorPrint
 
 let usage focus =
+  cp "\foTool for applying XSLT style sheets\f0."
   cp "\foxsltrx \fg-s \fcstylesheet.xslt \fg-f \fcdoc.xml \f0[\fg-o \fcoutfile\f0]"
   cp "   Apply an XSLT transform to an XML document. Supports EXSLT and other extensions."
   cp "\fg-v               \f0Verbose mode"
