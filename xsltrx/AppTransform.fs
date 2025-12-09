c3cb1ccb5694423be73d9d524438eb395542794f
ttelcl <9429819+ttelcl@users.noreply.github.com>
2025-12-09 15:22:10 +0200

    xsltrx: Adding -O option. Preparing for multi-stylesheet (pipeline) use

--- xsltrx/AppTransform.fs
+++ xsltrx/AppTransform.fs
@@ -12,16 +12,52 @@ open Mvp.Xml.Common.Xsl
 open ColorPrint
 open CommonTools
 
-type private Options = {
-  Stylesheet: string
+type private OutputDerivation = {
+  InputTail: string
+  OutputTail: string
+}
+
+type private InputOutput = {
   Input: string
   Output: string
-  EnableDocumentFunction: bool
 }
+
+type private Transformation = {
+  XsltFile: string
+  LoadedTransform: MvpXslTransform
+}
+
+type private Options = {
+  Stylesheet: string
+  EnableDocumentFunction: bool
+  InOutPairs: InputOutput list
+  OutputDerivations: OutputDerivation list
+}
+
+let private tryParseDerivation (derivation: string) =
+  let parts = derivation.Split(':')
+  if parts.Length <> 2 then
+    cp $"\frInvalid argument to '-O'. \f0Expecting a string with exactly one '\fo:\f0' but got '\fy{derivation}\f0'"
+    None
+  else
+    {
+      InputTail = parts[0]
+      OutputTail = parts[1]
+    } |> Some
+
+let private tryDeriveOutput derivations input =
+  let tryDerive (input: string) derivation =
+    if input.EndsWith(derivation.InputTail, StringComparison.OrdinalIgnoreCase) then
+      let head = input.Substring(0, input.Length - derivation.InputTail.Length)
+      Some(head + derivation.OutputTail)
+    else
+      None
+  derivations
+  |> Seq.choose (tryDerive input)
+  |> Seq.tryHead
 
 let private runTransform o =
   // see also https://github.com/devlooped/Mvp.Xml/blob/main/src/Mvp.Xml/Exslt/Xsl/MvpXslTransform.cs
-  let doc = new XPathDocument(o.Input)
   
   // To Do: extract stylesheet from document if o.StyleSheet is missing
 
@@ -35,17 +71,94 @@ let private runTransform o =
       fun trx xsltFile -> trx.Load(xsltFile)
   let createTransform xsltFile =
     let trx = new MvpXslTransform()
+    cp $"Loading stylesheet \fb{xsltFile}\f0."
     transformloader trx xsltFile
     trx
-  let transform = o.Stylesheet |> createTransform
-  do
-    use tw = o.Output |> startFile
-    let arglist = new XsltArgumentList()
-    let input = new XmlInput(doc)
-    let output = new XmlOutput(tw)
-    transform.Transform(input, arglist, output) |> ignore
-  o.Output |> finishFile
-  0
+  let stylesheets = [ o.Stylesheet ] // preparing for future pipeline
+  let pipeline =
+    stylesheets
+    |> List.map (fun ss -> {XsltFile = ss; LoadedTransform = ss |> createTransform})
+  // validate output method of intermediate stages and return the final output method
+  let rec getPipelineOutputMethod transformList =
+    match transformList with
+    | trx :: [] ->
+      trx.LoadedTransform.OutputSettings.OutputMethod |> Some
+    | trx :: rest ->
+      let outputMethod = trx.LoadedTransform.OutputSettings.OutputMethod
+      if outputMethod <> XmlOutputMethod.Xml then
+        cp $"\frError: Intermediate stylesheets must have output method 'XML', but got '\fb{outputMethod}\fr' in \f0'\fc{trx.XsltFile}\f0'."
+        None
+      else
+        rest |> getPipelineOutputMethod
+    | [] ->
+      cp "\frNo transformations in the pipeline\f0."
+      None
+  let lastOutputMethod = pipeline |> getPipelineOutputMethod
+  match lastOutputMethod with
+  | Some(lastOutputMethod) ->
+    let defaultExtension =
+      match lastOutputMethod with
+      | XmlOutputMethod.Text -> ".txt"
+      | XmlOutputMethod.Html -> ".html"
+      | _ -> ".out.xml" // avoid overwriting input, which most likely already has extension ".xml"
+    let resolveOutput pair =
+      if pair.Output |> String.IsNullOrEmpty then
+        let input = pair.Input |> Path.GetFileName
+        let output =
+          if o.OutputDerivations |> List.isEmpty then
+            // silently use the default
+            Path.ChangeExtension(input, defaultExtension)
+          else
+            let attempt = input |> tryDeriveOutput o.OutputDerivations
+            match attempt with
+            | Some(output) -> output
+            | None ->
+              // noisily use the default
+              cp $"\frWarning\f0: no '\fg-O\f0' rule matched '\fy{input}\f0'. Using the default output naming rule."
+              Path.ChangeExtension(input, defaultExtension)
+        {pair with Output = output}
+      else
+        pair
+    let pairs = o.InOutPairs |> List.map resolveOutput
+    let transformIntermediate trx (xin:XmlInput) =
+      let arglist = new XsltArgumentList()
+      let xout = trx.LoadedTransform.Transform(xin, arglist)
+      new XmlInput(xout)
+    let transformLast trx outname (xin:XmlInput) =
+      let arglist = new XsltArgumentList()
+      // PLACEHOLDER!
+      do
+        use tw = outname |> startFile
+        let xout = new XmlOutput(tw)
+        trx.LoadedTransform.Transform(xin, arglist, xout) |> ignore
+      outname |> finishFile
+    let rec transformPipeline outname xin pipeline =
+      match pipeline with
+      | trx :: [] ->
+        xin |> transformLast trx outname
+      | trx :: remainder ->
+        let xin2 = xin |> transformIntermediate trx
+        remainder |> transformPipeline outname xin2
+      | [] ->
+        failwith "Not expecting an empty pipeline here"
+    for pair in pairs do
+      let input = pair.Input
+      let output = pair.Output
+      cp $"Transforming \fg{input}\f0 to \fo{output}\f0."
+      let doc = new XPathDocument(input)
+      let xin = new XmlInput(doc)
+      pipeline |> transformPipeline output xin
+      //do
+      //  use tw = output |> startFile
+      //  let arglist = new XsltArgumentList()
+      //  let input = new XmlInput(doc)
+      //  let output = new XmlOutput(tw)
+      //  transform.Transform(input, arglist, output) |> ignore
+      //output |> finishFile
+    0
+  | None ->
+    // an error was printed already
+    1
 
 let run args =
   let rec parseMore o args =
@@ -57,36 +170,47 @@ let run args =
     | "-h" :: _ ->
       None
     | "-f" :: file :: rest ->
-      rest |> parseMore {o with Input = file}
+      let pair = {
+        Input = file
+        Output = null
+      }
+      rest |> parseMore {o with InOutPairs = pair :: o.InOutPairs}
     | "-s" :: stylesheet :: rest ->
       rest |> parseMore {o with Stylesheet = stylesheet}
     | "-o" :: outfile :: rest ->
-      rest |> parseMore {o with Output = outfile}
+      match o.InOutPairs with
+      | head :: others ->
+        let pair = {head with Output = outfile}
+        rest |> parseMore {o with InOutPairs = pair :: others}
+      | [] ->
+        cp $"\fr'fg-o \f0{outfile}\fr' applies to the preceding '\fg-f\fr', but there were none of those yet\f0."
+        None
     | "-doc" :: rest ->
-      // voodoo inferred from https://stackoverflow.com/a/77903294/271323
+      // Voodoo inferred from https://stackoverflow.com/a/77903294/271323
       AppContext.SetSwitch("Switch.System.Xml.AllowDefaultResolver", true);
       rest |> parseMore {o with EnableDocumentFunction = true}
+    | "-O" :: derivation :: rest ->
+      match derivation |> tryParseDerivation with
+      | None -> None
+      | Some(d) ->
+        rest |> parseMore {o with OutputDerivations = d :: o.OutputDerivations}
     | [] ->
       if o.Stylesheet |> String.IsNullOrEmpty then
         cp "\foNo stylesheet file (\fg-s\fo) given\f0."
         None
-      elif o.Input |> String.IsNullOrEmpty then
-        cp "\foNo input file (\fg-f\fo) given\f0."
-        None
-      elif o.Output |> String.IsNullOrEmpty then
-        // For now, require an output file
-        cp "\foNo output file (\fg-o\fo) given\f0."
+      elif o.InOutPairs |> List.isEmpty then
+        cp "\foNo input files (\fg-f\fo) given\f0."
         None
       else
-        o |> Some
+        {o with OutputDerivations = o.OutputDerivations |> List.rev} |> Some
     | x :: _ ->
       cp $"\frUnrecognized argument \f0'\fy{x}\f0'"
       None
   let oo = args |> parseMore {
     Stylesheet = null
-    Input = null
-    Output = null
+    InOutPairs = []
     EnableDocumentFunction = false
+    OutputDerivations = []
   }
   match oo with
   | Some(o) ->
--- xsltrx/Usage.fs
+++ xsltrx/Usage.fs
@@ -6,13 +6,17 @@ open ColorPrint
 
 let usage focus =
   cp "\foTool for applying XSLT style sheets\f0."
-  cp "\foxsltrx \fg-s \fcstylesheet.xslt \fg-f \fcdoc.xml \f0[\fg-o \fcoutfile\f0] [\fg-doc\f0]"
+  cp "\foxsltrx \fg-s \fcstylesheet.xslt \f0{\fg-f \fcdoc.xml\f0} [\fg-o \fcoutfile\f0] {\fg-O \fcinTail\fo:\fcoutTail\f0} [\fg-doc\f0]"
   cp "   Apply an XSLT transform to an XML document. Supports EXSLT and other extensions."
   cp "\fg-s \fcstylesheet.xslt\f0     The stylesheet"
-  cp "\fg-f \fcdoc.xml\f0             The document to transform"
-  cp "\fg-o \fcoutfile\f0             The output file. By default derived from the document (\fg-f\f0)"
+  cp "\fg-f \fcdoc.xml\f0             The document(s) to transform. Repeatable"
+  cp "\fg-o \fcoutfile\f0             The output file. By default derived from the preceding document (\fg-f\f0)"
   cp "\fg-doc\f0\fx                   Enable the XSLT \fodocument()\f0 function. Without this it is disabled."
   cp "\fx\fx\fx                       Even when given, only filesystem based documents are allowed."
+  cp "\fg-O \fcinTail\fo:\fcoutTail\f0      Add a rule to derive the output name from an input (\fg-f\f0) name."
+  cp "\fx\fx\fx                       If the input ends with \fcinTail\f0 then replace that with \fcoutTail\f0 and remove the path."
+  cp "\fx\fx\fx                       If no rule matches, the default rule that replaces the input extension with \fo.out.xml\f0 applies."
+  cp "\fx\fx\fx                       Example: '\fg-O \fc.xml\fo:\fc.out.xml\f0'"
   cp "\fg-v               \f0Verbose mode"
 
 
